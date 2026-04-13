6ffa215a55432bd9def673b63e218fe6490b1f29
Дима <dima@Dmytros-MacBook-Pro.local>
2026-04-13 19:48:34 +0200

    fixing: was fixed some bugs in diffrent files of project

--- server/src/Biotopes/model/biotopePrisma.js
+++ server/src/Biotopes/model/biotopePrisma.js
@@ -1,7 +1,5 @@
-import asyncErrorHandler from '../../ErrorHandlers/asyncErrorHandler.js';
-import * as BiotopeModel from '../model/biotopePrisma.js';
+import prisma from "../../utils/prisma.js";
 
-export const getBiotopes = asyncErrorHandler(async(res,req) => {
-    const biotopes = await BiotopeModel.getAllBiotopes();
-    res.status(200).json({status: "success", data: biotopes});
-})
+export const getAllBiotopes = async () => {
+    return await prisma.biotopes.findMany();
+};
