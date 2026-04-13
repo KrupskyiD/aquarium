4d1d7e18f216a23375434ad9af8c996c9946fd21
Дима <dima@Dmytros-MacBook-Pro.local>
2026-04-13 18:54:57 +0200

    feat: add prima model

---
+++ server/src/Aquarium/controllers/aquariumController.js
@@ -0,0 +1,7 @@
+// import asyncErrorHandler from '../../ErrorHandlers/asyncErrorHandler.js';
+// import * as AquariumModel from '../model/aquariumPrisma.js';
+//
+// export const getAll = asyncErrorHandler (async(req, res) =>
+//     const aquariums await AquariumModel.getAllAquariums(res.user.id);
+//     res.status(200).json(status: 'status: "success", data: aquariums');
+// )
---
+++ server/src/Aquarium/model/aquariumPrisma.js
@@ -0,0 +1,52 @@
+import prisma from "../../utils/prisma.js";
+
+// тут я отримую всі акваріумів, які є у клієнта
+export const getAllAquariums = async (userId) => {
+    return await prisma.aquarium.findMany({
+        where: { user_id: userId }
+    });
+}
+
+// отримую конкретний акваріум, з провіркою власника
+export const getAquariumById = async (id, userId) => {
+    return await prisma.aquarium.findFirst({
+        where: {
+            id: parseInt(id),
+            user_id: userId
+        },
+        })
+}
+
+// стоврюю новий акваріум
+export const createAquarium = async (data, userId) => {
+    return await prisma.aquarium.create({
+        data: {
+            name: data.name,
+            min_salt: data.min_salt,
+            max_salt: data.max_salt,
+            min_temp: data.min_temp,
+            max_temp: data.max_temp,
+            device_serial: data.device_serial,
+            user_id: userId,
+        }
+    })
+}
+
+//оновляю акваріум (додав user_id для безпеки, мб це лишнє)
+export const updateAquarium = async (id, data, userId) => {
+    return await prisma.aquarium.update({
+        where: {
+            id: parseInt(id)
+        },
+        data: data,
+    })
+}
+
+export const deleteAquarium = async (id,userId) => {
+    return await prisma.aquarium.delete({
+        where: {
+            id: parseInt(id),
+            user_id: userId
+        },
+    })
+}
