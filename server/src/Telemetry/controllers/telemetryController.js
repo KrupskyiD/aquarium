e24c35457e1a0acfa76848eac4e04271f5dce8d1
Denys Krupskyi <den.k.programming@gmail.com>
2026-04-16 14:26:29 +0200

    Added again deleted connecting between telemetry and DB

--- server/src/Telemetry/controllers/telemetryController.js
+++ server/src/Telemetry/controllers/telemetryController.js
@@ -1,10 +1,15 @@
 import asyncErrorHandler from '../../errorHandlers/asyncErrorHandler.js';
 import { saveMetricsToDB } from '../model/telemetryPrisma.js';
 import { getIO } from '../../../Socket/socket.js';
+import { customError } from '../../ErrorHandlers/customError.js';
 
 export const telemetryController = asyncErrorHandler(async (req, res, next) => {
     //getting the data object from gateway
     const data = req.body;
+
+    //saving data to database
+    const sendMetricsToDB = await saveMetricsToDB(data);
+    if(!sendMetricsToDB) return next(new customError('Your aqaurium did not find. Check if you created aquarium with this device', 404));
 
     //get only metricks from the package for sending to frontend
     const metrics = {
