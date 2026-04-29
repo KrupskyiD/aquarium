e941ad19d857c3b423c60b843b137380bebf7dc9
Denys Krupskyi <den.k.programming@gmail.com>
2026-04-29 12:29:50 +0200

    removed plus4U from mine npm config. Added unfunctional button for deleting. There's needed to add another icon + red color #E84B5A33.

--- client/src/features/detail/components/ButtonCard.jsx
+++ client/src/features/detail/components/ButtonCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ButtonCard = ({title, icon, onClick}) => {
   return (
-    <button onClick={onClick} className='w-full flex items-center justify-between bg-[#121A21] p-3 rounded-2xl border border-slate-700/50 hover:bg-slate-800/50 transition-colors text-left'>
+    <button onClick={onClick} className='w-full flex items-center justify-between bg-[#0C1A28] p-3 rounded-2xl border border-slate-700/50 hover:bg-slate-800/50 transition-colors text-left'>
         <div className="flex items-center gap-4">
         <div className="w-11 h-11 rounded-xl bg-slate-800/80 flex items-center justify-center text-slate-300">
           {icon}
