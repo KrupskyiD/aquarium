2ee62b34322fcdfb0479a7957cc402ca84f779ff
Denys Krupskyi <den.k.programming@gmail.com>
2026-04-30 16:38:12 +0200

    New and professional(mot mine:( ) metrics restriction. Storage for sent data, limits, and a function, which verificates new metrics.

--- gateway/gateway.js
+++ gateway/gateway.js
@@ -2,9 +2,8 @@ const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 const axios = require('axios');
 require('dotenv').config();
-import { saltRestriction, tempRestriction } from './dataController.js';
-import { sendSaltToServer, sendTempToServer } from './dataSender.js';
-
+import { sendDataToServer } from './controller/dataController.js';
+import { storage } from './storage/storage.js';
 
 const parser = new ReadlineParser({
     delimiter: '\r\n'
@@ -22,42 +21,38 @@ let port = new SerialPort({
 
 port.pipe(parser);
 
-//gateway memory for salt and temp
-let saltData = [];
-let tempData = [];
-
 parser.on('data',async function(data){
 
     try {
         //split data in array by ":"
         const parsedData = data.split(':');
 
-        //check if data are equal
-        const salt = await saltRestriction(saltData, parsedData);
-        const temp = await tempRestriction(tempData, parsedData);
-        
-        if(salt === 'SALT_NUMBERS_ARE_EQUAL'){
-            const sendSalt = await sendSaltToServer(JSON.stringify(salt));
+        if(parsedData.length === 3) {
+           //create object for sending to the server
+            const dataObject = {
+                device_serial: parsedData[0].trim(),
+                temperature: parseFloat(parsedData[1]),
+                salt: parseFloat(parsedData[2]),
+                // timestamp: new Date().toISOString(),
+            }
+            //verification restrictions
+            if(sendDataToServer(dataObject)){
+                //if it passed, it'll send data
+                await axios.post(process.env.BACKEND_URL, dataObject, {
+                    headers: {
+                        'X-API-KEY': dataObject.device_serial
+                    }
+                });
+            }
+            //updating storage
+            storage.salt = dataObject.salt;
+            storage.temperature = dataObject.temperature;
+            storage.timestamp = Date.now();
+
+            //if it haven't passed the verification, it just ignores new metrics
+        } else {
+            console.log('Failed to parse the string:', data);
         }
-        if(temp === 'TEMP_NUMBERS_ARE_EQUAL'){
-            const sendTemp = await sendTempToServer(JSON.stringify(temp));
-        }
-        
-        // if(parsedData.length === 3) {
-        //    const dataObject = {
-        //         device_serial: parsedData[0].trim(),
-        //         temperature: parseFloat(parsedData[1]),
-        //         salt: parseFloat(parsedData[2]),
-        //         // timestamp: new Date().toISOString(),
-        //     }
-        //     await axios.post(process.env.BACKEND_URL, dataObject, {
-        //         headers: {
-        //             'X-API-KEY': dataObject.device_serial
-        //         }
-        //     });
-        // } else {
-        //     console.log('Failed to parse the string:', data);
-        // }
 
     } catch (error) {
         console.error('Error while sending to the backend:', error.message);
