33bf2bbd4ff654539ad44d17a177e0be43b36c9d
Denys Krupskyi <den.k.programming@gmail.com>
2026-05-04 15:19:08 +0200

    Deploy. Settled Dockerfile for both: cleint and server, and ci/cd pipelines with runners. Using aws

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
 
 import io from 'socket.io-client'
-const BACKEND_URL = import.meta.env.BACKEND_URL || "http://localhost:3001";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3001";
 //getting user token for socket by main key from locall storage
 export function getToken() {
   const userToken = localStorage.getItem('saltguard.auth.session');
